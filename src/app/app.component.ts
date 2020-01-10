536f75fc2348223d50991c92de85bf8e8490ab49
Asiri Jayaweera <asandaruwan64@gmail.com>
2020-01-10 20:45:45 +0530

    Added state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, of, combineLatest } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+import { State, states } from './shared/State';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Dynamic-Filtering-rxjs';
+  states$: Observable<State[]>;
+  filteredStates$: Observable<State[]>;
+  filter: FormControl;
+  filter$: Observable<string>;
+
+  constructor() {
+    this.states$ = of(states);
+    this.filter = new FormControl('');
+    this.filter$ = this.filter.valueChanges.pipe(startWith(''));
+    this.filteredStates$ = combineLatest(this.states$, this.filter$).pipe(
+      map(([statesList, filterString]) => statesList.filter(state => state.name.indexOf(filterString) !== -1))
+    );
+  }
 }
